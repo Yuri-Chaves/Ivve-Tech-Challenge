394e09a68c834e41c5531958a907f509886601b6
Yuri-Chaves <Yuri.Chaves39@hotmail.com>
2024-06-30 17:45:55 -0300

    remove shadow

--- web/components/Paper.tsx
+++ web/components/Paper.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Paper({children}:React.PropsWithChildren<{}>) {
   return (
     <div className=" h-full
-      bg-white rounded-lg shadow-lg p-4 text-zinc-700
+      bg-white rounded-lg p-4 text-zinc-700
       dark:bg-slate-600 dark:text-zinc-100">
       {children}
     </div>
