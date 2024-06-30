864ecf44a919e58e4dd059ae24bee52cb8d3b1f8
Yuri-Chaves <Yuri.Chaves39@hotmail.com>
2024-06-29 23:03:45 -0300

    feat: Init Home page

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,11 +1,21 @@
-"use client"
+import ChuckFact from "@/components/ChuckFact";
 
+import {chuckIvveApiPT} from "../mocks/chuckIvveApi";
+import { ResizablePanel, ResizablePanelGroup } from "@/components/ui/resizable";
+import { Button } from "@/components/ui/button";
+import RequestConfigs from "@/components/RequestConfigs";
 
-
-export default function Home() {
+export default function Home() {  
   return (
-    <main >
-      Yuri Chaves
-    </main>
+    <section className="flex flex-col items-center justify-center min-h-screen px-4 py-12 sm:px-6 lg:px-8">
+      <ResizablePanelGroup direction="horizontal" className="sm:max-w-4xl gap-1">
+        <ResizablePanel defaultSize={70}>
+          <ChuckFact {...chuckIvveApiPT} />
+        </ResizablePanel>
+        <ResizablePanel defaultSize={30}>
+          <RequestConfigs />
+        </ResizablePanel>
+      </ResizablePanelGroup>
+    </section>
   );
 }
