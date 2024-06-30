d87d59f3754c79fbc9ef23659d6beaf47539d471
Yuri-Chaves <Yuri.Chaves39@hotmail.com>
2024-06-30 14:42:39 -0300

    refactor: Change category type

--- web/components/ChuckCategories.tsx
+++ web/components/ChuckCategories.tsx
@@ -11,19 +11,30 @@ const categoryScrollTitle: Record<TLanguage, string> = {
 export default function ChuckCategories() {
   const { language, panelProps } = useSiteLanguage()
 
+  const sortedCategories = panelProps.categories.slice(1).sort((a, b) => {
+    const aLabel = a.label[language].toLocaleLowerCase()
+    const bLabel = b.label[language].toLocaleLowerCase()
+
+    if (aLabel < bLabel) return -1
+    if (aLabel > bLabel) return 1
+    return 0
+  })
+
+  const shownCategories = [panelProps.categories[0], ...sortedCategories]
+
   return (
     <div className="flex flex-col">
       <p>{categoryScrollTitle[language]}</p>
       <ScrollArea className="h-36 md:h-48">
         <ToggleGroup type="single" className="flex flex-col gap-2" defaultValue="none">
-          {panelProps.categories[language].map((category) => (
+          {shownCategories.map((category) => (
             <ToggleGroupItem
-              key={category.label}
+              key={category.value}
               value={category.value}
               className="flex flex-row items-center w-full justify-start gap-1"
             >
               {category.icon}
-              <p>{category.label}</p>
+              <p>{category.label[language]}</p>
             </ToggleGroupItem>
           ))}
         </ToggleGroup>
--- web/services/LanguageSettings/Categories.tsx
+++ web/services/LanguageSettings/Categories.tsx
@@ -20,266 +20,158 @@ import {
 import { IoFastFood, IoBan } from "react-icons/io5"
 import { FaBookAtlas, FaBookBible } from "react-icons/fa6"
 
-export const categories: PanelProps['categories'] = {
-  br: [
+export const categories: PanelProps['categories'] = [
     {
-      label: "nenhuma",
+      label: {
+        br: "nenhuma",
+        es: "ninguna",
+        us: "none",
+      },
       icon: <IoBan />,
       value: "none",
     },
     {
-      label: "animal",
+      label: {
+        br: "animal",
+        es: "animal",
+        us: "animal",
+      },
       icon: <MdPets />,
       value: "animal",
     },
     {
-      label: "carreia",
+      label: {
+        br: "carreira",
+        es: "carrera",
+        us: "career",
+      },
       icon: <BiSolidBriefcase />,
       value: "career",
     },
     {
-      label: "celebridade",
+      label: {
+        br: "celebridade",
+        es: "celebridad",
+        us: "celebrity",
+      },
       icon: <MdStar />,
       value: "celebrity",
     },
     {
-      label: "ciência",
+      label: {
+        br: "ciência",
+        es: "ciencia",
+        us: "science",
+      },
       icon: <GiCorkedTube />,
       value: "science",
     },
     {
-      label: "cinema",
+      label: {
+        br: "cinema",
+        es: "cine",
+        us: "movie",
+      },
       icon: <MdLocalMovies />,
       value: "movie",
     },
     {
-      label: "comida",
+      label: {
+        br: "comida",
+        es: "comida",
+        us: "food",
+      },
       icon: <IoFastFood />,
       value: "food",
     },
     {
-      label: "dev",
+      label: {
+        br: "dev",
+        es: "dev",
+        us: "dev",
+      },
       icon: <MdLogoDev />,
       value: "dev",
     },
     {
-      label: "dinheiro",
+      label: {
+        br: "dinheiro",
+        es: "dinero",
+        us: "money",
+      },
       icon: <GiMoneyStack />,
       value: "money",
     },
     {
-      label: "esporte",
+      label: {
+        br: "esporte",
+        es: "deporte",
+        us: "sport",
+      },
       icon: <MdSportsFootball />,
       value: "sport",
     },
     {
-      label: "explícito",
+      label: {
+        br: "explícito",
+        es: "explícito",
+        us: "explicit",
+      },
       icon: <MdNoAdultContent />,
       value: "explicit",
     },
     {
-      label: "história",
+      label: {
+        br: "história",
+        es: "historia",
+        us: "history",
+      },
       icon: <FaBookAtlas />,
       value: "history",
     },
     {
-      label: "moda",
+      label: {
+        br: "moda",
+        es: "moda",
+        us: "fashion",
+      },
       icon: <GiAmpleDress />,
       value: "fashion",
     },
     {
-      label: "música",
+      label: {
+        br: "música",
+        es: "música",
+        us: "music",
+      },
       icon: <GiMusicalScore />,
       value: "music",
     },
     {
-      label: "política",
+      label: {
+        br: "política",
+        es: "política",
+        us: "political",
+      },
       icon: <GiCongress />,
       value: "political",
     },
     {
-      label: "religião",
+      label: {
+        br: "religião",
+        es: "religión",
+        us: "religion",
+      },
       icon: <FaBookBible />,
       value: "religion",
     },
     {
-      label: "viagem",
+      label: {
+        br: "viagem",
+        es: "viaje",
+        us: "travel",
+      },
       icon: <GiCommercialAirplane />,
       value: "travel",
     },
-  ],
-  es: [
-    {
-      label: "ninguna",
-      icon: <IoBan />,
-      value: "none",
-    },
-    {
-      label: "animal",
-      icon: <MdPets />,
-      value: "animal",
-    },
-    {
-      label: "carrera",
-      icon: <BiSolidBriefcase />,
-      value: "career",
-    },
-    {
-      label: "celebridad",
-      icon: <MdStar />,
-      value: "celebrity",
-    },
-    {
-      label: "ciencia",
-      icon: <GiCorkedTube />,
-      value: "science",
-    },
-    {
-      label: "cine",
-      icon: <MdLocalMovies />,
-      value: "movie",
-    },
-    {
-      label: "comida",
-      icon: <IoFastFood />,
-      value: "food",
-    },
-    {
-      label: "deporte",
-      icon: <MdSportsFootball />,
-      value: "sport",
-    },
-    {
-      label: "dev",
-      icon: <MdLogoDev />,
-      value: "dev",
-    },
-    {
-      label: "dinero",
-      icon: <GiMoneyStack />,
-      value: "money",
-    },
-    {
-      label: "explícito",
-      icon: <MdNoAdultContent />,
-      value: "explicit",
-    },
-    {
-      label: "historia",
-      icon: <FaBookAtlas />,
-      value: "history",
-    },
-    {
-      label: "moda",
-      icon: <GiAmpleDress />,
-      value: "fashion",
-    },
-    {
-      label: "música",
-      icon: <GiMusicalScore />,
-      value: "music",
-    },
-    {
-      label: "política",
-      icon: <GiCongress />,
-      value: "political",
-    },
-    {
-      label: "religión",
-      icon: <FaBookBible />,
-      value: "religion",
-    },
-    {
-      label: "viaje",
-      icon: <GiCommercialAirplane />,
-      value: "travel",
-    }
-  ],
-  us: [
-    {
-      label: "none",
-      icon: <IoBan />,
-      value: "none",
-    },
-    {
-      label: "animal",
-      icon: <MdPets />,
-      value: "animal",
-    },
-    {
-      label: "career",
-      icon: <BiSolidBriefcase />,
-      value: "career",
-    },
-    {
-      label: "celebrity",
-      icon: <MdStar />,
-      value: "celebrity",
-    },
-    {
-      label: "dev",
-      icon: <MdLogoDev />,
-      value: "dev",
-    },
-    {
-      label: "explicit",
-      icon: <MdNoAdultContent />,
-      value: "explicit",
-    },
-    {
-      label: "fashion",
-      icon: <GiAmpleDress />,
-      value: "fashion",
-    },
-    {
-      label: "food",
-      icon: <IoFastFood />,
-      value: "food",
-    },
-    {
-      label: "history",
-      icon: <FaBookAtlas />,
-      value: "history",
-    },
-    {
-      label: "money",
-      icon: <GiMoneyStack />,
-      value: "money",
-    },
-    {
-      label: "movie",
-      icon: <MdLocalMovies />,
-      value: "movie",
-    },
-    {
-      label: "music",
-      icon: <GiMusicalScore />,
-      value: "music",
-    },
-    {
-      label: "political",
-      icon: <GiCongress />,
-      value: "political",
-    },
-    {
-      label: "religion",
-      icon: <FaBookBible />,
-      value: "religion",
-    },
-    {
-      label: "science",
-      icon: <GiCorkedTube />,
-      value: "science",
-    },
-    {
-      label: "sport",
-      icon: <MdSportsFootball />,
-      value: "sport",
-    },
-    {
-      label: "travel",
-      icon: <GiCommercialAirplane />,
-      value: "travel",
-    },
-  ]
-}
+]
